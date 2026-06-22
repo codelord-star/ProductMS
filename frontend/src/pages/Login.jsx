078db3a91821bb059cc1af7cb9b39e510ef8787e
codelord-star <jacobhagee11@gmail.com>
2026-06-22 11:12:55 +0300

    product

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 
 function Login(){
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await api.get("users/");
+            const users = res.data;
+            const user = users.find(u => u.email === email && u.password === password);
+            if (user) {
+                localStorage.setItem("user", JSON.stringify(user));
+                alert("Login successful");
+                navigate("/");
+            } else {
+                alert("Invalid email or password");
+            }
+        } catch (error) {
+            console.error("Login error:", error);
+            alert("An error occurred while logging in");
+        }
+    };
+
     return(
         <div className="container mt-3">
             <div className="card shadow">
@@ -10,9 +35,9 @@ function Login(){
                             <h1>Login</h1>
                         </div>
                         <div>
-                            <form action="">
-                                <input type="email" id="email" name="email" className="form-control mb-3" placeholder="Email"/>
-                                <input type="password" id="password" name="password" className="form-control mb-3" placeholder="Password"/>
+                            <form onSubmit={loginUser}>
+                                <input type="email" id="email" name="email" className="form-control mb-3" required placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                <input type="password" id="password" name="password" className="form-control mb-3" required placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                                 <button type="submit" className="btn btn-success">Login</button>
                             </form>
                         </div>
