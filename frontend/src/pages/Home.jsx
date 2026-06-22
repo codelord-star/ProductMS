a3cd7df16c375860bf64aeda0cef94e9d1fd0acf
codelord-star <jacobhagee11@gmail.com>
2026-06-22 11:49:36 +0300

    product

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
 import ProductCard from "../components/productcard";
+import image1 from "../assets/images/pic4.jpeg"
+import image2 from "../assets/images/pic1.jpeg"
+import image3 from "../assets/images/pic2.jpeg"
 
 function Home() {
 
@@ -40,8 +43,29 @@ function Home() {
   return (
 
     <div className="container mt-5">
+      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={image1} class="d-block w-100" alt="Image 1"/>
+          </div>
+          <div class="carousel-item">
+            <img src={image2} class="d-block w-100" alt="Image 2"/>
+          </div>
+          <div class="carousel-item">
+            <img src={image3} class="d-block w-100" alt="Image 3"/>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
 
-      <h2 className="mb-4">
+      <h2 className="mb-3 mt-3">
         Available Products
       </h2>
 
